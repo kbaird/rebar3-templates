815c416eccca9696571ac66b90782bd9a6287ed9
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-10-31 17:16:01 -0700

    Basic operations that I don't hate.

--- eunit.erl
+++ eunit.erl
@@ -1,4 +1,4 @@
--module('{{name}}_test').
+-module('{{topic}}_test').
 -author('{{author_name}}').
 
 -include_lib("eunit/include/eunit.hrl").
@@ -6,28 +6,20 @@
 lone_test() ->
     ?assertEqual(true, write_some_real_ones).
 
-% TOPIC
+% {{topic}}
 
-topic_setup()     -> ok.
-topic_teardown(_) -> ok.
+{{topic}}_setup()     -> ok.
+{{topic}}_teardown(_) -> ok.
 
-topic_test_() ->
-    {setup, fun topic_setup/0,
-            fun topic_teardown/1,
+{{topic}}_test_() ->
+    {setup, fun {{topic}}_setup/0,
+            fun {{topic}}_teardown/1,
             [
-                fun topic_xxx_case/0,
-                fun topic_yyy_case/0,
-                fun topic_zzz_case/0
+                fun {{topic}}_initial_case/0
             ]
     }.
 
-topic_xxx_case() ->
-    ?assertEqual(true, write_some_real_ones).
-
-topic_yyy_case() ->
-    ?assertEqual(true, write_some_real_ones).
-
-topic_zzz_case() ->
+{{topic}}_initial_case() ->
     ?assertEqual(true, write_some_real_ones).
 
 %%% PRIVATE FUNCTIONS
